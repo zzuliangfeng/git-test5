bf8c2df3a536d79c1526f19452df5f64b457d320
raymond <liangfenglam@163.com>
2022-05-08 21:26:00 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,9 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
