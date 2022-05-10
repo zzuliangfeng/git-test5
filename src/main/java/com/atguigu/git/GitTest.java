5b9c64ec0c5da54d5f7cd5380a45ddbb9e970885
zzuliangfeng <898942868@qq.com>
2022-05-10 09:29:43 +0800

    git push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("git test!");
+        System.out.println("git push test!");
     }
 }
